e6e694fb42783f7e45e72d07e84084feaa6bee97
unknown <rockcena483@gmail.com>
2023-11-05 20:42:23 +0530

    msg1

--- src/pages/S3StaticWebpage.jsx
+++ src/pages/S3StaticWebpage.jsx
@@ -3,10 +3,12 @@ import { Navigate } from "react-router-dom";
 function S3StaticWebpage() {
   return (
     // <Navigate to="http://sample-static-website-avi.s3-website.ap-south-1.amazonaws.com/" />
-    <iframe
-      src="http://sample-static-website-avi.s3-website.ap-south-1.amazonaws.com/"
-      title="S3 static webpage"
-    ></iframe>
+    <>
+      <iframe
+        src="http://sample-static-website-avi.s3-website.ap-south-1.amazonaws.com/"
+        title="S3 static webpage"
+      ></iframe>
+    </>
   );
 }
 

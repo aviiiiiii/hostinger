b88f6d217111cac5bd503d82206a25e2a68555bf
Aviiiiii <rockcena483@gmail.com>
2024-04-01 22:28:49 +0530

    new file

---
+++ src/pages/ThreeRadio.jsx
@@ -0,0 +1,20 @@
+function ThreeRadio() {
+  return (
+    <div class="radio-buttons">
+    <label>
+      <input type="radio" name="option" value="option1">
+      Option 1
+    </label>
+    <label>
+      <input type="radio" name="option" value="option2">
+      Option 2
+    </label>
+    <label>
+      <input type="radio" name="option" value="option3">
+      Option 3
+    </label>
+  </div>
+  );
+}
+
+export default ThreeRadio;

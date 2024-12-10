2f01a0b6332887463063f3841e2caf0f282c4d1f
Aviiiiii <rockcena483@gmail.com>
2024-12-11 00:11:34 +0530

    m2

--- src/pages/eventsManagement/HomePage.jsx
+++ src/pages/eventsManagement/HomePage.jsx
@@ -6,41 +6,41 @@ function EventsHomePage() {
     <div className="eventsHomePage">
       <NavBar />
       <div className="description">
-        <p>Event Management has been crafting extraordinary events across the country for the past 2 years. From electrifying DJ nights and serene camping trips to enlightening book fairs, vibrant parties, and captivating concerts, we do it all. With a passion for creativity and a knack for innovation, we bring unique ideas to life.</p>
+        <p>Discover a world of opportunities to learn, grow, and connect. From tech innovations to creative arts, our curated events are designed to inspire and empower you. Whether you're a professional, a hobbyist, or just curious, there's something here for everyone. Join us to explore exciting workshops, insightful sessions, and hands-on experiences. Start your journey with us today!</p>
       </div>
       <div className="sliderImageDiv">
         <section className="slider_container">
           <section className="slider">
             <div className="slide one">
-              <img src={require("../../images/EventManagement/concert.jpg")} alt="" />
+              <img src={require("../../images/EventManagement/publcSpeaking.jpg")} alt="" />
               <span className="caption">
-                Concert
+                Public Speaking
               </span>
             </div>
             <div className="slide two">
-              <img src={require("../../images/EventManagement/camping.jpg")} alt="" />
+              <img src={require("../../images/EventManagement/retroEvent.jpg")} alt="" />
               <span className="caption">
-                Camping
+                Retro Event
               </span>
             </div>
             <div className="slide three">
-              <img src={require("../../images/EventManagement/party.jpg")} alt="" />
+              <img src={require("../../images/EventManagement/photography.jpg")} alt="" />
               <span className="caption">
-                Party
+                Photography
               </span>
             </div>
-            {/* <div className="slide four">
-        <img src="https://cdn.pixabay.com/photo/2023/01/06/13/33/city-7701251_960_720.jpg" alt=""/>
-        <span className="caption">
-          slide four
-        </span>
-      </div>
-      <div className="slide four">
-        <img src="https://cdn.pixabay.com/photo/2023/01/10/00/30/swan-7708580__340.jpg" alt=""/>
-        <span className="caption">
-          slide five
-        </span>
-      </div> */}
+            <div className="slide four">
+              <img src={require("../../images/EventManagement/fitness.jpg")} alt="" />
+              <span className="caption">
+                Fitness
+              </span>
+            </div>
+            <div className="slide four">
+              <img src={require("../../images/EventManagement/circus.jpg")} alt="" />
+              <span className="caption">
+                Circus
+              </span>
+            </div>
           </section>
         </section>
       </div>
@@ -48,28 +48,28 @@ function EventsHomePage() {
         <footer className="section footer-classNameic context-dark bg-image">
           <div className="container">
             <div className="row row-30">
-              
+
               <hr />
               <br></br>
               <div className="col-md-4">
                 <h5>Contacts</h5>
                 <dl className="contact-list">
                   <dt>Address:</dt>
-                  <dd>798 South Park Avenue, New York, USA</dd>
+                  <dd>No:1311, Palavakkam Beach, Sholinganallur, Chennai, Tamil Nadu, India</dd>
                 </dl>
               </div>
               <div className="col-md-4">
                 <dl className="contact-list">
                   &nbsp;
                   <dt>email:</dt>
-                  <dd><a href="mailto:#">compagny@email.com</a></dd>
+                  <dd><a href="mailto:#">aviiiiii1311@email.com</a></dd>
                 </dl>
               </div>
               <div className="col-md-4">
                 <dl className="contact-list">
                   &nbsp;
                   <dt>phones:</dt>
-                  <dd><a href="tel:#">1.555.652.258</a> <span>or</span> <a href="tel:#">1.55.652.268</a>
+                  <dd><a href="tel:#">+91 9998887770</a>
                   </dd>
                 </dl>
               </div>
@@ -81,7 +81,7 @@ function EventsHomePage() {
             <div className="col"><a className="social-inner" href="#"><span className="icon mdi mdi-twitter"></span><span>Twitter</span></a></div>
             <div className="col"><a className="social-inner" href="#"><span className="icon mdi mdi-youtube-play"></span><span>Youtube</span></a></div>
           </div>
-                <p className="rights" style={{margin:'0px'}}><span>©  </span><span className="copyright-year">2024</span><span> </span><span>Company</span><span> - </span><span>All Rights Reserved.</span></p>
+          <p className="rights" style={{ margin: '0px' }}><span>©  </span><span className="copyright-year">2024</span><span> </span><span>Company</span><span> - </span><span>All Rights Reserved.</span></p>
         </footer>
       </div>
     </div>
